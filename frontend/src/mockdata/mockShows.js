d2eeb9d5ff0ae202ecb6b2f9014c9cc21088d4e7
Luc Dreibholz <luc.dreibholz@gmx.net>
2024-02-08 18:34:33 +0100

    lint test

--- frontend/src/mockdata/mockShows.js
+++ frontend/src/mockdata/mockShows.js
@@ -109,6 +109,7 @@ export const mockShows = [
         titleJP: 'Ao No Exorcist',
         description:
             'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut tempora ad sed, voluptatibus nam asperiores vel ipsa harum aperiam nemo error maiores praesentium repellendus rem labore necessitatibus obcaecati corrupti similique.',
-        url: '/assets/cover.jpg',
+url: '/assets/cover.jpg',
+hello: "",
     },
 ];
