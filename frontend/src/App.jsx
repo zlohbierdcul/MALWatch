7b87bfc5d8779dc7a803b962787138ba896b20b8
Luc Dreibholz <luc.dreibholz@gmx.net>
2024-02-14 17:07:09 +0100

    husky test 6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,36 @@
 // React
-import { useState } from 'react';
+import { useState } from 'react'
 
 // Components
-import NavigationBar from '@Components/NavigationBar/NavigationBar';
-import MobileNavigationBar from '@Components/MobileNavigationBar/MobileNavigationBar';
-import StarRating from '@Components/StarRating/StarRating';
+import NavigationBar from '@Components/NavigationBar/NavigationBar'
+import MobileNavigationBar from '@Components/MobileNavigationBar/MobileNavigationBar'
+import StarRating from '@Components/StarRating/StarRating'
 
 // Views
-import LoginView from '@Views/LoginView/LoginView';
-import HomeView from '@Views/HomeView/HomeView';
+import LoginView from '@Views/LoginView/LoginView'
+import HomeView from '@Views/HomeView/HomeView'
 
 // Styling
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles'
 
 // Utils
-import { theme } from '@Utils/css/theme';
-
+import { theme } from '@Utils/css/theme'
 
 const App = () => {
-    const [loggedIn, setLoggedIn] = useState(true);
-    return (
-        <ThemeProvider theme={theme}>
-            {!loggedIn && <LoginView setLoggedIn={setLoggedIn}></LoginView>}
-            {loggedIn && (
-                <>
-                    <NavigationBar></NavigationBar>
-                    <HomeView></HomeView>
-                    <StarRating rating={4.3}></StarRating>
-                    <MobileNavigationBar></MobileNavigationBar>
-                </>
-            )}
-        </ThemeProvider>
-    );
+  const [loggedIn, setLoggedIn] = useState(true)
+  return (
+    <ThemeProvider theme={theme}>
+      {!loggedIn && <LoginView setLoggedIn={setLoggedIn}></LoginView>}
+      {loggedIn && (
+        <>
+          <NavigationBar></NavigationBar>
+          <HomeView></HomeView>
+          <StarRating rating={4.3}></StarRating>
+          <MobileNavigationBar></MobileNavigationBar>
+        </>
+      )}
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
